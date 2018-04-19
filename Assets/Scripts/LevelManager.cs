a6528c1eba9efd83c7eb33690bc51f7f44594876
Taylor Jang <taylorjang@hotmail.com>
2018-04-19 08:29:20 -0500

    init commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        //Debug.Log("Start - LoadNextLevel");
+    }
+
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+     }
+
+    public void LoadNextLevel()
+    {
+        //Debug.Log("before - LoadNextLevel");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Debug.Log("after - LoadNextLevel");
+    }
+
+    public void Quit(string name)
+    {
+        Application.Quit();
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (levelMusicChangeArray[level] != null)
+        {
+            audioSource.clip = levelMusicChangeArray[level];
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+}
